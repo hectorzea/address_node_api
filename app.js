ee54cecdf3ba3fd0cd70966542a6fa305279e188
Hector <hzea@inclusionservices.com>
2019-06-10 23:03:20 -0300

    added weather calls

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const LocationService = require("./services/LocationService");
+const WeatherService = require("../weather_api/weather");
 const arguments = require('yargs').options({
     address: {
         alias: 'addr',
@@ -6,11 +7,50 @@ const arguments = require('yargs').options({
     }
 }).argv;
 
-LocationService.getAddress(arguments.address).then((oResponse) => {
-    console.log(oResponse);
-}).catch((error) => {
-    console.log(error)
+// LocationService.getAddress(arguments.address).then((oResponse) => {
+//     let oAddress = oResponse.shift();
+//     if (oAddress){
+//         WeatherService.getWeather(oAddress.lat, oAddress.lon).then( (sWeather) => {
+//             if (sWeather){
+//                 console.log(`El Clima de ${oAddress.name} es de ${sWeather}`)
+//             }else{
+//                 console.log(`No se encontró clima para ${oAddress.name}`)
+//             }
+//         })
+//     }else{
+//         console.log(`No se encontró la dirección ${arguments.address}`)
+//     }
+// }).catch((error) => {
+//     console.log(error)
+// })
+
+//METODO CON AWAIT
+
+const getWeatherByLatLong = async () => {
+    let aAddress = await LocationService.getAddress(arguments.address);
+    let oAddress = aAddress.shift();
+    if (oAddress) {
+        let sWeather = await WeatherService.getWeather(oAddress.lat, oAddress.lon);
+        if (sWeather) {
+            return `El Clima de ${oAddress.name} es de ${sWeather}`;
+        }
+    } else {
+        console.log(`No se encontró la dirección ${arguments.address}`)
+    }
+};
+
+getWeatherByLatLong().then((sResponse) => {
+    console.log(sResponse);
+}).catch((e) => {
+    console.log(e);
 })
+
+// WeatherService.getWeather(40.50000, 1.1111).then( (sWeather) => {
+//     console.log(sWeather);
+// }).catch( (e) => {
+//     console.log(e);
+// })
+
 
 // const sEncodedUrl = encodeURI(arguments.address);
 // console.log(sEncodedUrl);
